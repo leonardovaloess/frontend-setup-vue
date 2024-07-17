d27858d7136604b52558f8774e4c24a32bf6aff5
Leonardo Valões <leonardoberlandadevaloes@gmail.com>
2024-07-17 10:24:52 -0300

    vuetify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import "./assets/main.scss";
+import "vuetify/styles";
 
+import { createVuetify } from "vuetify/lib/framework.mjs";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
 
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 
