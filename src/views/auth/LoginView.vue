209dc0168ff674645ac11f9523910e22f5e5ebc0
Leonardo Berlanda de Valões <leonardoberlandadevaloes@MacBook-Air-de-Leonardo.local>
2024-11-23 11:43:33 -0300

    login view v2

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -6,15 +6,17 @@ import LoginImage from "@/components/LoginImage.vue";
 
 <template>
   <div class="flex w-full h-screen justify-center relative">
-    <div class="p-5 flex flex-col justify-center">
+    <div class="p-5 flex flex-col justify-center w-full items-center">
       <div class="w-full flex justify-center">
         <img src="../../assets/png/Logo.png" alt="" width="150px" />
       </div>
 
-      <h1 class="text-large text-3xl mt-5 mb-5 font-medium">Login</h1>
-      <p class="text-base md:text-xl font-light mb-5">
-        Preencha seus dados corretamente para acessar.
-      </p>
+      <div class="w-[100%] md:w-[500px]">
+        <h1 class="text-large text-3xl mt-5 mb-5 font-medium">Login</h1>
+        <p class="text-base md:text-xl font-light mb-5">
+          Preencha seus dados corretamente para acessar.
+        </p>
+      </div>
       <div class="w-[100%] md:w-[500px]">
         <div class="flex flex-col w-full mb-3">
           <label>Email:</label>
@@ -44,6 +46,9 @@ import LoginImage from "@/components/LoginImage.vue";
           >
         </div>
       </div>
+    </div>
+    <div class="w-full hidden md:flex items-center justify-center bg-blue-500">
+      <LoginImage />
     </div>
   </div>
 </template>
